784e15818fcecd03f43c1256e9a9944243e5ccdc
Hamza65523 <102131334+Hamza65523@users.noreply.github.com>
2022-10-05 11:39:13 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -21,22 +21,22 @@ const worker = createWorker();
     TextGenrator()
   },[selectedImg])
   return (
-    <div className="App h-[100vh] bg-blue-500">
+    <div className="App  bg-blue-500">
       <div className=" text-white text-center ">
         <h1 className='text-2xl font-[500] capitalize  pt-8'>Image to  text</h1>
 
         <div className="relative mt-4 w-[150px] mx-auto my-0 ">
-          <input className='text-white bg-red-500 rounded-lg w-[250px] ' type="file" name="" id="" onChange={(e)=>handlerChange(e)} />
+          <input className='text-white bg-red-500 rounded-lg w-[100px] max-w-[250px]' type="file" name="" id="" onChange={(e)=>handlerChange(e)} />
         </div>
-       <div className="flex px-8 py-8 items-center gap-4 ">
+       <div className="flex px-8 py-8 items-center gap-4 flex-wrap justify-center">
        {selectedImg&& (
-          <div className="mt-4  max-w-[1200px] h-[400px]">
-            <img src={URL.createObjectURL(selectedImg)} className='border-4  border-green-300' alt="" />
+          <div className="mt-4  max-w-[1200px] ">
+            <img src={URL.createObjectURL(selectedImg)} className='border-4 max-h-[700px]  max-w-[400px] border-green-300' alt="" />
           </div>
         )}
         {textResult&&(
-          <div className="mt-4 max-w-[600px] h-[400px]">
-            <p className='text-white border-green-400 border-4 px-2 py-2'>{textResult}</p>
+          <div className="mt-4 max-w-[600px] ">
+            <p className='text-white max-h-[700px] md:max-[500px] border-green-400 border-4 px-2 py-2'>{textResult}</p>
           </div>
         )}
        </div>
